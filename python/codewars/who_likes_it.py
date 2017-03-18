ec320f26a0e1668bc92e8418ee17449e03265317
AlexaVB <alexaverityballantine@Alexas-MacBook-Pro.local>
2017-03-18 16:59:11 +0000

    wip

--- python/codewars/who_likes_it.py
+++ python/codewars/who_likes_it.py
@@ -1,10 +1,16 @@
-names = []
+names = ["Jon", "Alexa", "Phil"]
 
 
 def likes(names):
-    if names == 0:
+    if names == []:
         print("no one likes this")
-    elif names == 1:
-        print(names + " likes this")
+    elif len(names) == 1:
+        print(names[0] + " likes this")
+    elif len(names) == 2:
+        print(names[0] + " and " + names[1] + " likes this")
     else:
-		print(likes)
+        for i in names:
+            print(", ".join(names))
+        return
+
+likes(names)
