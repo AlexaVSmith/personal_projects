e0c9b7d930ad52a09f9cf9062fdb4b577f45d273
AlexaVB <alexaverityballantine@Alexas-MacBook-Pro.local>
2017-03-18 14:54:27 +0000

    solution

--- python/codewars/highest_and_lowest.py
+++ python/codewars/highest_and_lowest.py
@@ -1,3 +1,12 @@
+numbers = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"
+
+
 def high_and_low(numbers):
-    # ...
-    return numbers
+    split_str = numbers.split(" ")
+    num_li = map(int, split_str)
+    sort = sorted(num_li)
+    first, last = [sort[-1], sort[0]]
+    result = str(first) + " " + str(last)
+    return(result) 
+
+print(high_and_low(numbers))
